7572ea35bfaf7623623d0a10b113f8a00ef624ca
Arslnmrv <arslanmerve1619@gmail.com>
2022-02-04 17:28:50 +0300

    d/8

--- cypress/integration/US_017/US_017.js
+++ cypress/integration/US_017/US_017.js
@@ -1,48 +1,104 @@
 Cypress.on('uncaught:exception', (err, runnable) => {
+   
+    return false
+  });
+  
+  // "baseUrl": "https://www.gmibank.com",
+  // "integrationFolder": "cypress/integration/api-tests"
+ 
+  import { Given } from "cypress-cucumber-preprocessor/steps";
+  
+  Given('user is on GMI Bank page',()=>{
+  
+    cy.visit('https://gmibank.com/');
+  
+  })
 
-  return false
-});
+  Then('clicks on the profile icon at the top right and then click on the Sign In button',()=>{
 
-// "baseUrl": "https://www.gmibank.com",
-// "integrationFolder": "cypress/integration/api-tests"
+    cy.get('#account-menu > .dropdown-toggle').click();
+    cy.get('#login-item').click();
 
-import { Given } from "cypress-cucumber-preprocessor/steps";
+  })
+  
+  Then('enters the required information and click sign in',()=>{
 
-Given('user is on GMI Bank page', () => {
+    cy.get('#username').type('Team68Admin');
+    cy.get('#password').type('Team68+');
+    cy.get('.btn-primary').click();
+     
+  })
 
-  cy.visit('https://gmibank.com/');
+  Then('after logging in, clicks the Administration icon at the top right and then click the User management button',()=>{
 
-})
+    cy.get('#admin-menu > .dropdown-toggle').click();
+    cy.get('[href="/admin/user-management"] > span').click()
+    
+  })
+ 
+  Then('activates a user in the Users list',()=>{
 
-Then('clicks on the profile icon at the top right and then click on the Sign In button', () => {
+    cy.get('#ira > :nth-child(4) > .btn').click();
+    cy.get('#ira > .text-right > .btn-group > .btn-info').click();
+    cy.get(':nth-child(2) > .badge').should('have.text' , 'Activated')
+  })
 
-  cy.get('#account-menu > .dropdown-toggle').click();
-  cy.get('#login-item').click();
+  Then('activates a employee in the employees list',()=>{
 
-})
+    cy.get('#admin-menu > .dropdown-toggle').click({ multiple: true })
+    cy.get('.dropdown-menu > .active').click()
+    cy.get('#kelvin > :nth-child(4) > .btn').click();
+    cy.get('#kelvin > .text-right > .btn-group > .btn-info').click();
+    cy.get('.badge > span').should('have.text' , 'Activated')
 
+  
+  })
 
-Then('enters the required information and click sign in', () => {
+  Then('activates a manager in the managers list',()=>{
+    cy.get('#admin-menu > .dropdown-toggle').click({ multiple: true })
+    cy.get('.dropdown-menu > .active').click()
+    cy.get('#benny > :nth-child(4) > .btn').click()
+    cy.get('#benny > .text-right > .btn-group > .btn-info').click()
+    cy.get(':nth-child(2) > .badge').should('have.text' , 'Activated')
+  
+  })
 
-  cy.get('#username').type('Team68Admin');
-  cy.get('#password').type('Team68+');
-  cy.get('.btn-primary').click();
+  Then('activates a admin in the admins list',()=>{
+    cy.get('#admin-menu > .dropdown-toggle').click({ multiple: true })
+    cy.get('.dropdown-menu > .active').click()
+    cy.get('#michael > :nth-child(4) > .btn').click()
+    cy.get('#michael > .text-right > .btn-group > .btn-info').click()
+    cy.get(':nth-child(2) > .badge').should('have.text' , 'Activated')
+ 
+  })
 
-})
+  Then('clicks admin view buttons',()=>{
+    cy.get('#admin-menu > .dropdown-toggle').click({ multiple: true })
+    cy.get('.dropdown-menu > .active').click()
+    cy.viewport(2500 ,1500)
+    cy.get('#sherlockholmes > .text-right > .btn-group > .btn-info').click()
+    cy.get('h2').should('be.visible')
+    
+  })
 
-Then('after logging in, clicks the Administration icon at the top right and then click the User management button', () => {
+  Then('clicks admin edit buttons',()=>{
+    cy.get('#admin-menu > .dropdown-toggle').click({ multiple: true })
+    cy.get('.dropdown-menu > .active').click()
+    cy.viewport(2500 ,1500)
+    cy.get('#bbell > .text-right > .btn-group > .btn-primary').click()
+    cy.get('h1 > span').should('be.visible')
+    cy.get('.btn-info').click()
+    cy.get('#murtaza > .text-right > .btn-group > .btn-primary').click()
+    cy.get('h1 > span').should('be.visible')
+  })
 
-  cy.get('#admin-menu > .dropdown-toggle').click();
-  cy.get('[href="/admin/user-management"] > span').click();
-
-})
-
-
-Then('activates a user in the Users list', () => {
-
-  cy.get('#ira > :nth-child(4) > .btn').click();
-
-  cy.get('#ira > .text-right > .btn-group > .btn-info').click();
-  cy.wait(5000)
-  cy.get(':nth-child(2) > .badge').should('have.text', 'Activated')
-})
+  Then('clicks admin delete buttons',()=>{
+    cy.get('#admin-menu > .dropdown-toggle').click({ multiple: true })
+    cy.get('.dropdown-menu > .active').click()
+    cy.viewport(2500 ,1500)
+    cy.get('#musterilerlogin > .text-right > .btn-group > .btn-danger').click()
+    cy.get('.modal-title').should('be.visible')
+    cy.get('.modal-footer > .btn-danger').click()
+    cy.get('.Toastify__toast').should('be.visible')
+    
+  })
